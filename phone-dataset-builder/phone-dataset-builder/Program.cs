2aeb030c260497585bec7208063d567bb0213f13
Arwin Neil Baichoo <arwinneil@gmail.com>
2017-05-07 13:32:32 +0400

    Getting and displaying all phone brands and details

--- phone-dataset-builder/phone-dataset-builder/Program.cs
+++ phone-dataset-builder/phone-dataset-builder/Program.cs
@@ -30,7 +30,7 @@ namespace phone_dataset_builder
 
             string line;
             bool table = false;
-
+            int formatter = 1;
             while ((line = sr.ReadLine()) != null)
 
             {
@@ -38,10 +38,21 @@ namespace phone_dataset_builder
                     break;
 
                 if (table == true && line != "")
-                {
-                    line = line.Substring((line.IndexOf(".php>") + 5), ((line.IndexOf("<br>")) - (line.IndexOf(".php>") + 5)));
 
-                    Console.Write(line + ",");
+                {
+                    string temp_brand = line.Substring((line.IndexOf(".php>") + 5), ((line.IndexOf("<br>")) - (line.IndexOf(".php>") + 5)));
+                    string temp_model_no = line.Substring((line.IndexOf("<span>") + 6), ((line.IndexOf(" devices")) - (line.IndexOf("<span>") + 6)));
+                    string temp_url = "http://www.gsmarena.com/" + line.Substring((line.IndexOf("href=") + 5), ((line.IndexOf(".php>") + 4) - (line.IndexOf("href=") + 5)));
+
+                    phone_brand Phone = new phone_brand(temp_brand, temp_model_no, temp_url);
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write(Phone.brand);
+                    Console.ResetColor();
+                    Console.Write(":" + Phone.model_no + " devices");
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.Write(" url:" + Phone.url + "\n");
+                    Console.ResetColor();
                 }
 
                 if (line.IndexOf("<table>") == 0)
